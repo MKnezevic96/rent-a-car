d70c1cf3f8a43f63e29fc2e1e7dc7d3cf3cc52d5
MKnezevic96 <miaknezevic5@gmail.com>
2020-06-22 01:04:33 +0200

    Fix enums

--- Agent/agentski_bekend/src/main/java/com/rent_a_car/agentski_bekend/controller/AdvertisementController.java
+++ Agent/agentski_bekend/src/main/java/com/rent_a_car/agentski_bekend/controller/AdvertisementController.java
@@ -114,6 +114,7 @@ public class AdvertisementController {
             carsService.save(c);
             return ResponseEntity.ok().build();
         }catch (Exception e){
+            e.printStackTrace();
         }
         return ResponseEntity.status(400).build();
     }
