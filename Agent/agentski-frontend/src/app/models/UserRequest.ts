538c188cbc992c0592bd84fa62e3e0f89a1e7ec2
antonelaparovic <antonelaparovic11@gmail.com>
2020-06-24 18:26:51 +0200

    front angular validation on register

--- Agent/agentski-frontend/src/app/models/UserRequest.ts
+++ Agent/agentski-frontend/src/app/models/UserRequest.ts
@@ -7,4 +7,17 @@ export class UserRequest {
     name : string;
     adress : string;
     number : string;
+
+
+
+    constructor (firstname:string, lastname:string, email:string, password:string, name:string, adress: string, number: string  ) {
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.email=email;
+        this.password=password;
+        this.isSelected="User";
+        this.name=name;
+        this.adress=adress;
+        this.number=number;
+    }
 }
